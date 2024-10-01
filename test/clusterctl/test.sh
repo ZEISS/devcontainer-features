11e78228b37bcebd58f32b09b619a2358848da0c
Sebastian Döll <sebastian@katallaxie.dev>
2024-10-01 11:22:25 +0000

    fix: check clusterctl version

--- test/clusterctl/test.sh
+++ test/clusterctl/test.sh
@@ -39,7 +39,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" clusterctl --version
+check "version" clusterctl version
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
